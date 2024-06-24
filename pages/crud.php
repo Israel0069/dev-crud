1dc38a86f0b7b8ad3efbabcfebac596af37802dc
Israel0069.email <iraqi0069@gmail.com>
2024-06-24 14:49:54 +0800

    adding data table

--- pages/crud.php
+++ pages/crud.php
@@ -1,4 +1,5 @@
-<!DOCTYPE html>
+
+.<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -8,6 +9,9 @@
     <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.0/js/bootstrap.bundle.min.js"></script>
+   <script src="https://cdn.datatables.net/2.0.8/js/dataTables.js"></script>
+   <script src="https://cdn.datatables.net/2.0.8/js/dataTables.bootstrap5.js"></script>
     <style>
         .wrapper{
             width: 600px;
@@ -44,8 +48,11 @@
     </form>
   </div>
 </nav>
+
 <body>
+
     <div class="wrapper">
+        
         <div class="container-fluid">
             <div class="row">
                 <div class="col-md-12">
@@ -61,7 +68,7 @@
                     $sql = "SELECT * FROM employees";
                     if($result = $pdo->query($sql)){
                         if($result->rowCount() > 0){
-                            echo '<table class="table table-bordered table-striped">';
+                            echo '<table id="example" class="table table-striped">';
                                 echo "<thead>";
                                     echo "<tr>";
                                         echo "<th>#</th>";
@@ -107,8 +114,13 @@
                     unset($pdo);
                     ?>
                 </div>
+                <script>
+                    
+   new DataTable('#example');
+</script>
             </div>        
         </div>
     </div>
+ 
 </body>
 </html>
